31caf159a7cbcaf8d84d32d17e53c8d0ae6a3fb7
cgwang1580 <cgwang1580@gmail.com>
2020-07-22 16:52:11 +0800

    添加FFmpeg库和头文件，打印各个模块版本号及编译选项，开源协议

--- FFmpegSample/app/src/main/cpp/native-lib.cpp
+++ FFmpegSample/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,55 @@
 #include <jni.h>
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_chauncy_ffmpegsample_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+extern "C" {
+	#include "libavcodec/version.h"
+	#include "libavcodec/avcodec.h"
+	#include "libavformat/version.h"
+	#include "libavutil/version.h"
+	#include "libavfilter/version.h"
+	#include "libswresample/version.h"
+	#include "libswscale/version.h"
+};
+
+#include "LogAndroid.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+JNIEXPORT jstring JNICALL
+Java_com_chauncy_ffmpegsample_MainActivity_getFFmpegVersion(JNIEnv *env, jobject thiz)
+{
+	// TODO: implement getFFmpegVersion()
+	char strVersion[1024 * 4]{0};
+	strcat(strVersion, "libavcodec version: ");
+	strcat(strVersion, AV_STRINGIFY(LIBAVCODEC_VERSION));
+
+	strcat(strVersion, "\nlibavformat version: ");
+	strcat(strVersion, AV_STRINGIFY(LIBAVFORMAT_VERSION));
+
+	strcat(strVersion, "\nlibavutil version: ");
+	strcat(strVersion, AV_STRINGIFY(LIBAVUTIL_VERSION));
+
+	strcat(strVersion, "\nlibavfilter version: ");
+	strcat(strVersion, AV_STRINGIFY(LIBAVFILTER_VERSION));
+
+	strcat(strVersion, "\nlibswresample version: ");
+	strcat(strVersion, AV_STRINGIFY(LIBSWRESAMPLE_VERSION));
+
+	strcat(strVersion, "\nlibswscale version: ");
+	strcat(strVersion, AV_STRINGIFY(LIBSWSCALE_VERSION));
+
+	strcat(strVersion, "\navcodec_configuration: ");
+	strcat(strVersion, avcodec_configuration());
+
+	strcat(strVersion, "\navcodec_license: ");
+	strcat(strVersion, avcodec_license());
+
+	LOGD("Java_com_chauncy_ffmpegsample_MainActivity_getFFmpegVersion strVersion: %s", strVersion);
+	return env->NewStringUTF(strVersion);
 }
+
+#ifdef __cplusplus
+}
+#endif
